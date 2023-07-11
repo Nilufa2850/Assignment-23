1328c72b7be3b9617d2be0d9efe31e50d043a137
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-11 18:59:00 +0530

    Create Question-04.cpp

---
+++ Question-04.cpp
@@ -0,0 +1,16 @@
+// 4. Write a C++ program to calculate the area of a circle
+
+#include <iostream>
+using namespace std ;
+
+int main ()
+{
+    int r ;
+    float A ;
+    cout<<"Enter radius(r) of a circle : ";
+    cin>>r ;
+    A = 3.14*r*r ;
+    cout<<"Area of circle is "<<A ;
+    return 0 ;
+
+}
