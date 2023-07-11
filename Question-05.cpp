27300b7d11914adf8315df968f43391eb943b2dc
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-11 19:17:15 +0530

    Create Question-05.cpp

---
+++ Question-05.cpp
@@ -0,0 +1,13 @@
+// 5. Write a C++ program to calculate the volume of a cuboid.
+
+#include<iostream>
+using namespace std ;
+
+int main ()
+{
+    float l , w , h ;
+    cout<<"Enter Length(l) , width (w) , height(h) : ";
+    cin>>l>>w>>h ;
+    cout<<"Volume of cuboid is "<<l*w*h ;
+    return 0 ;
+}
