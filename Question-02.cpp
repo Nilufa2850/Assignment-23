c4c215c77f1c71f30abfcf6a8d4a5a398af6520c
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-11 18:51:10 +0530

    Create Question-02.cpp

---
+++ Question-02.cpp
@@ -0,0 +1,18 @@
+//2. Write a C++ program to print Hello on the first line and MySirG on the second line using endl.
+
+#include<iostream>
+using namespace std ;
+int main ()
+{
+    cout<<"Hello"<<endl<<"MySirG" ;
+    cout << endl ;
+
+    //Another way
+
+    cout<<"Hello" ;
+    cout<<endl ;
+    cout<<"MySirG" ;
+    
+    return 0 ;
+
+}
