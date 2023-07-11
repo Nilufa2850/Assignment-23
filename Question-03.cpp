ec03aebcb90470a781128ac40f97318c8db0d708
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-07-11 18:54:01 +0530

    Create Question-03.cpp

---
+++ Question-03.cpp
@@ -0,0 +1,12 @@
+// 3. Write a C++ program to calculate the sum of two numbers.
+
+#include<iostream> 
+using namespace std ;
+int main ()
+{
+    int x,y ;
+    cout<<"Enter two numbers : " ;
+    cin>>x>>y ;
+    cout<<"sum of "<<x<<" and "<<y<<" is "<<x+y ;
+    return 0 ;
+}
